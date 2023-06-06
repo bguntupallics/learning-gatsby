1bdb3fe7bcdff1513f5614ea8f257ccc3e99ee0e
bguntupalli <bharguntupalli@gmail.com>
2023-06-06 15:29:59 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `learning-gatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
